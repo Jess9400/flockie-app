1fd162f6176aa06fdf356a2a660806d4bc10cf6f
Jess9400 <023logistica@gmail.com>
2026-07-15 22:51:22 +0530

    Revert "Chats: unified list + desktop two-pane rail + unread nav badge"
    This reverts commit 4e6852efa9b6ebda206d22345d1938f6f1a2ec93.

--- src/app/(app)/chats/loading.tsx
+++ src/app/(app)/chats/loading.tsx
@@ -2,11 +2,12 @@ import { SkeletonLine, SkeletonListRow } from "@/components/skeletons";
 
 export default function ChatsLoading() {
   return (
-    <main className="mx-auto w-full max-w-2xl px-5 pb-12 pt-6 lg:hidden">
+    <main className="mx-auto w-full max-w-2xl px-5 pb-12 pt-6">
       <SkeletonLine className="h-8 w-28" />
+      <SkeletonLine className="mt-2 h-4 w-64" />
 
-      {/* Filter chips */}
-      <SkeletonLine className="mt-4 h-7 w-48" />
+      {/* Tabs pill */}
+      <SkeletonLine className="mt-5 h-10 w-64" />
 
       <div className="mt-6">
         <SkeletonLine className="h-3.5 w-56" />
